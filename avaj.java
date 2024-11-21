f77f066b533b74ef5dec66712193b9ceda6478c0
cmr <kumarsravan1227@gmail.com>
2024-11-21 13:54:52 +0530

    first commit

--- avaj.java
+++ avaj.java
@@ -1,5 +1,5 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("hello wrldssssssssssssssssss");
+        System.out.println("hello wrldzzzzzzzzzzzzzzzzzzzzz");
     }
 }
